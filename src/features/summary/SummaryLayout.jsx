3e0119679c6a8d0ad8ee821161f4359665fc27a0
Vladimir <nacver3@gmail.com>
2026-07-26 09:11:44 +0200

    styling summary

--- src/features/summary/SummaryLayout.jsx
+++ src/features/summary/SummaryLayout.jsx
@@ -13,7 +13,8 @@ const StyledSummaryLayout = styled.div`
   background-color: var(--color-grey-200);
   border: 1px solid var(--color-brand-600);
   border-radius: 4px;
-  gap: ${(props) => (props.$isMobile === 'mobile' ? '2px' : '1rem')};
+  row-gap: ${(props) => (props.$isMobile === 'mobile' ? '2px' : '1rem')};
+  column-gap: 0px;
   padding: ${(props) =>
     props.$isMobile === 'mobile' ? '2px 0px' : '1rem 0rem'};
 `;
@@ -35,11 +36,13 @@ const StyledOveralHeading = styled.h1`
   align-items: center;
 `;
 const StyledGrillLayout = styled.div`
-  width: 95%;
+  width: 100%;
   height: 95%;
   background-color: var(--color-indigo-700);
   border: 1px solid var(--color-yellow-600);
   border-radius: 4px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
   color: var(--color-indigo-100);
   grid-row: 2/2;
   grid-column: 1/1;
@@ -60,16 +63,20 @@ const StyledImgBox = styled.div`
   width: 100%;
   align-self: center;
   justify-self: center;
-  background-color: var(--color-silver-100);
+  background-color: var(--color-indigo-700);
   border-radius: 4px;
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
   grid-column: 2/2;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 4px 4px 0px 0px;
 `;
 const StyledImg = styled.img`
-  width: 58%;
+  width: 150px;
   height: auto;
+  border: solid 8px var(--color-indigo-100);
   border-radius: 4px;
 `;
 function SummaryLayout() {
@@ -82,23 +89,23 @@ function SummaryLayout() {
   console.log(delivery);
   const grilKg = delivery.reduce(
     (acc, curr) => (curr.gril_pack === 'kg' ? acc + curr.grill_quant : acc),
-    0
+    0,
   );
   const grilPola = delivery.reduce(
     (acc, curr) => (curr.gril_pack === '0.5kg' ? acc + curr.grill_quant : acc),
-    0
+    0,
   );
   const grilKom = delivery.reduce(
     (acc, curr) => (curr.gril_pack === 'ком' ? acc + curr.grill_quant : acc),
-    0
+    0,
   );
   const tradKg = delivery.reduce(
     (acc, curr) => (curr.trad_pack === 'kg' ? acc + curr.trad_quant : acc),
-    0
+    0,
   );
   const tradPola = delivery.reduce(
     (acc, curr) => (curr.trad_pack === '0.5kg' ? acc + curr.trad_quant : acc),
-    0
+    0,
   );
   const cream = delivery.reduce((acc, curr) => acc + curr.cream_quant, 0);
   console.log(grilKg, grilKom, grilPola, tradKg, tradPola, cream);
@@ -135,7 +142,7 @@ function SummaryLayout() {
         </StyledOveralHeading>
       </StyledGrillLayout>
       <StyledImgBox>
-        <StyledImg src="/gril.jpg" alt="gril" />
+        <StyledImg src='/gril.jpg' alt='gril' />
       </StyledImgBox>
       <StyledTradLayout>
         <StyledOveralHeading
@@ -160,7 +167,7 @@ function SummaryLayout() {
         </StyledOveralHeading>
       </StyledTradLayout>
       <StyledImgBox>
-        <StyledImg src="/trad.jpg" alt="trad" />
+        <StyledImg src='/trad.jpg' alt='trad' />
       </StyledImgBox>
       <StyledCreamLayout>
         <StyledOveralHeading
@@ -175,7 +182,7 @@ function SummaryLayout() {
         </StyledOveralHeading>
       </StyledCreamLayout>
       <StyledImgBox>
-        <StyledImg src="/cream.jpg" alt="cream" />
+        <StyledImg src='/cream.jpg' alt='cream' />
       </StyledImgBox>
     </StyledSummaryLayout>
   );
